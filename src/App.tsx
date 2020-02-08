636dcff32811cdf2891a38d2e8431efeedb942f3
JasonDollar <elrey33@gmail.com>
2020-02-08 21:42:06 +0100

    added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import { 
+  BrowserRouter as Router, Route, Redirect, Switch, 
+} from 'react-router-dom'
 
 import Users from './user/pages/Users'
 import NewPlaces from './places/pages/NewPlaces'
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" >
-          <NewPlaces />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    </Router>
-  )
-}
+const App: React.FC = () => (
+  <Router>
+    <Switch>
 
-export default App;
+      <Route path="/" exact>
+        <Users />
+      </Route> 
+      <Route path="/places/new">
+        <NewPlaces />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  </Router>
+)
+ 
+export default App
