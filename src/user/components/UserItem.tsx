dc49d75fbc47e371de791bfa295ae961dac81f7e
JasonDollar <elrey33@gmail.com>
2020-02-09 15:34:49 +0100

    UserItem component worked on

--- src/user/components/UserItem.tsx
+++ src/user/components/UserItem.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { Item } from '../userInterfaces'
+import styles from './UserItem.module.scss'
 
 type Props = Item 
   
 
 const UserItem: React.FC<Props> = ({ 
   id, name, placeCount, image, 
-}) => {
-  console.log(id, name, placeCount, image)
-
-  return (
-    <div />
-  )
-}
+}) => (
+  <li className={styles.listItem}>
+    <div className={styles.image}>
+      <img src={image} alt={name} />
+    </div>
+    <div className={styles.info}>
+      <h2>{name}</h2>
+      <h3>
+        {placeCount} {placeCount === 1 ? 'Place' : 'Places'}
+      </h3>
+    </div>
+  </li>
+)
 
 export default UserItem
