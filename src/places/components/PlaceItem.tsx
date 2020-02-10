df65900c3e6e133a91b6ba446966d6b014e959e0
JasonDollar <elrey33@gmail.com>
2020-02-10 16:28:53 +0100

    Modal component, using modal in PlaceItem

--- src/places/components/PlaceItem.tsx
+++ src/places/components/PlaceItem.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Place, Location } from '../placesInterfaces'
 import Card from '../../shared/components/UIElements/Card'
 import Button from '../../shared/components/FormElements/Button'
+import Modal from '../../shared/components/UIElements/Modal'
 
 import styles from './PlaceItem.module.scss'
 
@@ -12,24 +13,46 @@ interface Props extends Place {
 
 const PlaceItem: React.FC<Props> = ({
   imageUrl, title, address, description, id,
-}) => (
-  <li className={styles.placeItem}>
-    <Card classNameProp={styles.content}>
-      <div className={styles.image}>
-        <img src={imageUrl} alt={title} />
-      </div>
-      <div className={styles.info}>
-        <h2>{title}</h2>
-        <h3>{address}</h3>
-        <p>{description}</p>
-      </div>
-      <div className={styles.actions}>
-        <Button inverse>VIEW ON MAP</Button>
-        <Button to={`/places/${id}`}>EDIT</Button>
-        <Button danger>DELETE</Button>
-      </div>
-    </Card>
-  </li>
-)
+}) => {
+  const [showMap, setShowMap] = useState(false)
+
+  const openMapHandler = () => setShowMap(true)
+
+  const closeMapHandler = () => setShowMap(false)
+
+  return (
+    <>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={address}
+        contentClass={styles.modalContent}
+        footerClass={styles.modalActions}
+        footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
+      >
+        <div className={styles.mapContainer}>
+          <h2>THE MAP</h2>
+        </div>
+      </Modal>
+      <li className={styles.placeItem}>
+        <Card classNameProp={styles.content}>
+          <div className={styles.image}>
+            <img src={imageUrl} alt={title} />
+          </div>
+          <div className={styles.info}>
+            <h2>{title}</h2>
+            <h3>{address}</h3>
+            <p>{description}</p>
+          </div>
+          <div className={styles.actions}>
+            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+            <Button to={`/places/${id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </>
+  )
+}
 
 export default PlaceItem
---
+++ src/shared/components/UIElements/Modal.tsx
@@ -0,0 +1,68 @@
+import React, { SyntheticEvent } from 'react'
+import ReactDOM from 'react-dom'
+import { CSSTransition } from 'react-transition-group'
+import Backdrop from './Backdrop'
+
+import styles from './Modal.module.scss'
+
+interface OverlayProps {
+  children?: React.ReactNode
+  footer?: React.ReactNode
+  classNameProp?: string
+  headerClass?: string
+  contentClass?: string
+  footerClass?: string
+  header?: string
+  style?: React.CSSProperties
+  onSubmitHandler?: () => void
+}
+
+interface ModalProps extends OverlayProps {
+  show: boolean
+  onCancel: () => void
+}
+
+const ModalOverlay: React.FC<OverlayProps> = ({
+  classNameProp, style, headerClass, header, onSubmitHandler, contentClass, children, footerClass, footer,
+}) => {
+  const content = (
+    <div className={`${styles.modal} ${classNameProp}`} style={style}>
+      <header className={`${styles.header} ${headerClass}`}>
+        <h2>{header}</h2>
+      </header>
+      <form onSubmit={onSubmitHandler || ((e: SyntheticEvent) => {
+        e.preventDefault()
+      })}
+      >
+        <div className={`${styles.content} ${contentClass}`}>
+          {children}
+        </div>
+        <footer className={`${styles.footer} ${footerClass}`}>
+          {footer}
+        </footer>
+      </form>
+    </div>
+  )
+  return ReactDOM.createPortal(content, document.getElementById('modal-hook'))
+}
+
+const Modal: React.FC<ModalProps> = props => {
+  's'
+
+  return (
+    <>
+      {props.show && <Backdrop onClick={props.onCancel} />}
+      <CSSTransition
+        in={props.show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      >
+        <ModalOverlay {...props} />
+      </CSSTransition>
+    </>
+  )
+}
+
+export default Modal
